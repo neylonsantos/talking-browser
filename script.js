ba8a4d118655a3dd8760adfd5a2f9a31b3cec5f9
Neylon Santos <neylonssantos@gmail.com>
2023-06-23 15:51:26 -0300

    create js script

---
+++ script.js
@@ -0,0 +1,24 @@
+let btn = document.querySelector(".falar-btn")
+let input = document.querySelector(".text-input")
+
+btn.addEventListener("click", () =>{
+  speakBrowser()
+})
+
+input.addEventListener("keypress", function(event) {
+  if (event.key === "Enter") {
+    speakBrowser()
+  }
+})
+
+function speakBrowser() {
+  let text = document.querySelector(".text-input").value
+  let voice = new SpeechSynthesisUtterance(text)
+
+  voice.lang = "pt-BR"
+  voice.pitch = 1
+  voice.volume = 1
+  voice.rate = 1
+
+  speechSynthesis.speak(voice)
+}
